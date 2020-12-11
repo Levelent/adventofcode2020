e1804d0e5fa5a4349d188e8f5c9c21763c05d928
Levelent <keegan.rimmington@gmail.com>
2020-12-11 14:45:35 +0000

    day 11 split over more functions

--- day11/seat_sighting.py
+++ day11/seat_sighting.py
@@ -1,31 +1,38 @@
-def sighted_seats(prev_rs, next_rs, s_r, s_c):
-    to_change = prev_rs[s_r][s_c]
-    if to_change == ".":
-        return False
+def can_see_occupied(prev_rs, r_source, c_source, r_dir, c_dir):
+    r_dist = 0
+    c_dist = 0
+    while True:
+        r_dist += r_dir
+        c_dist += c_dir
+        if not 0 <= r_source + r_dist < len(prev_rs) or not 0 <= c_source + c_dist < len(prev_rs[0]):
+            return False
+        to_check = prev_rs[r_source + r_dist][c_source + c_dist]
+        if to_check != ".":
+            return to_check == "#"
 
+
+def count_occupied(prev_rs, r_source, c_source):
     occupied = 0
     for r_dir in range(-1, 2):
         for c_dir in range(-1, 2):
             if r_dir == c_dir == 0:
                 continue
-            r_dist = 0
-            c_dist = 0
-            while True:
-                r_dist += r_dir
-                c_dist += c_dir
-                if not 0 <= s_r + r_dist < len(prev_rs) or not 0 <= s_c + c_dist < len(prev_rs[0]):
-                    break
-                to_check = prev_rs[s_r + r_dist][s_c + c_dist]
-                if to_check != ".":
-                    if to_check == "#":
-                        occupied += 1
-                    break
+            occupied += can_see_occupied(prev_rs, r_source, c_source, r_dir, c_dir)
+    return occupied
+
+
+def change_seat(prev_rs, next_rs, r_source, c_source):
+    to_change = prev_rs[r_source][c_source]
+    if to_change == ".":
+        return False
+
+    occupied = count_occupied(prev_rs, r_source, c_source)
 
     if occupied == 0 and to_change == "L":
-        next_rs[s_r][s_c] = "#"
+        next_rs[r_source][c_source] = "#"
         return True
     elif occupied >= 5 and to_change == "#":
-        next_rs[s_r][s_c] = "L"
+        next_rs[r_source][c_source] = "L"
         return True
     return False
 
@@ -39,7 +46,7 @@ while swaps > 0:
     swaps = 0
     for i in range(len(rows)):
         for j in range(len(rows[0])):
-            swaps += sighted_seats(rows, next_rows, i, j)
+            swaps += change_seat(rows, next_rows, i, j)
     rows = next_rows
 
 total = 0
